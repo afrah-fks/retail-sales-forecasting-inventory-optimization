b3eedd4081a577aca729100ad608f065f01ef402
Afrah <afrahfks@gmail.com>
2026-04-17 16:43:06 +0530

    Updated outputs, visualization and added inventory table image

--- src/visualization.py
+++ src/visualization.py
@@ -1,16 +1,23 @@
 import matplotlib.pyplot as plt
+import os
 
 def plot_sales(df, forecast):
-    plt.figure(figsize=(10,5))  # optional (better size)
+    # Create images folder if not exists
+    os.makedirs("images", exist_ok=True)
+
+    plt.figure(figsize=(10,5))
 
     plt.plot(df['date'], df['sales'], label='Actual Sales')
     plt.plot(df['date'], forecast['forecast'], label='Forecasted Sales')
 
-    plt.title("Retail Sales Forecast vs Actual Sales")  # ✅ TITLE
-    plt.xlabel("Date")  # ✅ X-axis label
-    plt.ylabel("Sales")  # ✅ Y-axis label
+    plt.title("Retail Sales Forecast vs Actual Sales")
+    plt.xlabel("Date")
+    plt.ylabel("Sales")
 
     plt.legend()
     plt.grid(True)
+
+    # ✅ SAVE IMAGE
+    plt.savefig("images/forecast_plot.png")
 
     plt.show()
